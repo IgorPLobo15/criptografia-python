1a240fc68ebe38e337b59e83663116b83e8a3afd
Igor Pereira Lobo <112497910+IgorPLobo15@users.noreply.github.com>
2023-03-24 12:19:09 -0300

    Listas

--- criptografia.py
+++ criptografia.py
@@ -1,15 +1,12 @@
-import collections
-
-alfabeto = collections.deque('abcdefghijklmnopqrstuvwxyz')
-alfabeto_rotacionado = alfabeto.copy()
-alfabeto_rotacionado.rotate(3)
-mensagem = input('Coloque sua Mensagem:')
-mensagem_decifrada = ''
+chave = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+chave_revezamento = ['X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
+mensagem = (input('Coloque sua mensagem:'))
+mensagem_criptografada = ''
 for letra in mensagem:
-    if letra in alfabeto:
-        posicao_alfabeto = alfabeto.index(letra)
-        mensagem_decifrada += alfabeto_rotacionado[posicao_alfabeto]
+    if letra in chave:
+        posicao_alfabeto = chave.index(letra)
+        mensagem_criptografada += chave_revezamento[posicao_alfabeto]
     else:
-        mensagem_decifrada += letra
+        mensagem_criptografada += letra
 
-print((mensagem_decifrada).replace('ã','8'))
+print(mensagem_criptografada)
--- descriptografia.py
+++ descriptografia.py
@@ -1,15 +1,13 @@
-import collections
-
-alfabeto = collections.deque('abcdefghijklmnopqrstuvwxyz')
-alfabeto_rotacionado = alfabeto.copy()
-alfabeto_rotacionado.rotate(-3)
-mensagem = input('Coloque sua Mensagem:')
-mensagem_decifrada = ''
+chave = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+chave_revezamento = ['d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c']
+chave_revezamento=   ['d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c']
+mensagem = (input('Coloque sua mensagem criptografada:'))
+mensagem_descriptografada = ''
 for letra in mensagem:
-    if letra in alfabeto:
-        posicao_alfabeto = alfabeto.index(letra)
-        mensagem_decifrada += alfabeto_rotacionado[posicao_alfabeto]''' '''''
+    if letra in chave:
+        posicao_alfabeto = chave.index(letra)
+        mensagem_descriptografada += chave_revezamento[posicao_alfabeto]
     else:
-        mensagem_decifrada += letra
+        mensagem_descriptografada += letra
 
-print((mensagem_decifrada).replace('8','ã'))
+print(mensagem_descriptografada)
